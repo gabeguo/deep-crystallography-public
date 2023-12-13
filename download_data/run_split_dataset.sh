adb52d0c4a90b2b15bf7021fbaa15cb011b302ce
Gabriel Guo <gzg2104@columbia.edu>
2023-12-13 17:07:27 -0500

    Update run_split_dataset.sh to filter by stability

--- download_data/run_split_dataset.sh
+++ download_data/run_split_dataset.sh
@@ -5,6 +5,7 @@ do
         --xrd_data_src /home/gabeguo/data/replicate_crystallography/${system}/xrds \
         --charge_data_dst /home/gabeguo/data/replicate_crystallography/${system}_split/charges \
         --xrd_data_dst /home/gabeguo/data/replicate_crystallography/${system}_split/xrds \
-        --train_percent 90 \
-        --val_percent 5
+        --stable_elems /home/gabeguo/data/replicate_crystallography/STABLE_material_info/${system^}_formulas.json \
+        --train_percent 80 \
+        --val_percent 10
 done
